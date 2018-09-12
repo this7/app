c9c0609ed03d143310f84fd9b31e19d4faa0fa63
qinuoyun <qinuoyun@qq.com>
2018-09-12 17:18:05 +0800

    v3.1.0 1.优化了配置文件的支持 2.优化了版本迭代 3.优化了JS-SDK引擎 4.新增了微信授权功能 5.新增定义函数库

---
+++ server/extends/function.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * This7 Frame
+ * @Author: qinuoyun
+ * @Date:   2018-09-12 17:16:45
+ * @Last Modified by:   qinuoyun
+ * @Last Modified time: 2018-09-12 17:17:23
+ */
+
+if (!function_exists('demo')) {
+    function demo($value = '') {
+        # code...
+    }
+}
