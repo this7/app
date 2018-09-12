3bca631bf58a3e8a82370c37a0dca384b49b0bc3
qinuoyun <qinuoyun@qq.com>
2018-09-12 17:12:13 +0800

    Revert "v3.1.0"
    This reverts commit 03568097866a44cfb861fa28a9ceb962d384e537.

--- server/connector/debugad42f6697b035b7580e4fef93be20b4d.php
+++ server/connector/debugad42f6697b035b7580e4fef93be20b4d.php
@@ -2,7 +2,7 @@
 namespace server\connector;
 use \this7\framework\staticize;
 
-class debugad42f6697b035b7580e4fef93be20b4d extends staticize {
+class debugad42f6697b035b7580e4fef93be20b4d extends staticize{
     public static function getAppAccessor() {
         return 'debug';
     }
--- server/connector/qrcode4441cd7e6b665842bd3d8b390dd9c3aa.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace server\connector;
-use \this7\framework\staticize;
-
-class qrcode4441cd7e6b665842bd3d8b390dd9c3aa extends staticize{
-    public static function getAppAccessor() {
-        return 'qrcode';
-    }
-}
